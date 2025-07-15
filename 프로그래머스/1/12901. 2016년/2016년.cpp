6d9ba125f64b4125f2ea2be01e18d4121e07a65d
byby3534 <82497387+byby3534@users.noreply.github.com>
2025-07-15 10:05:10 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 string solution(int a, int b) {
     vector<int> month = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    vector<string> day = { "THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED" };
-    int SumDay = b;
+    vector<string> day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+    int SumDay = b - 1;
     
     for (int i = 0; i < a - 1; i++)
     {
