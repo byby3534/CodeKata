c323069fdbe240a68c1bb8fcc35b79e18796e2e0
byby3534 <82497387+byby3534@users.noreply.github.com>
2025-08-20 10:43:13 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -1,39 +1,22 @@
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void my_swap(int& a, int& b)
-{
-    int tmp = a;
-    a = b;
-    b = tmp;
-}
-
-vector<int> my_sort(vector<int> array, int begin, int end)
-{
-    vector<int> sub(array.begin() + begin - 1, array.begin() + end);
-
-    for (int i = 0; i < sub.size() - 1; i++)
-    {
-        for (int j = 0; j < sub.size() - i - 1; j++)
-        {
-            if (sub[j] > sub[j + 1])
-            {
-                my_swap(sub[j], sub[j + 1]);
-            }
-        }
-    }
-    return sub;
-}
-
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    
-    for (vector<int> v : commands)
+    vector<int> tmpArr;
+    for (vector<int> command : commands)
     {
-        vector<int> tmpArr = my_sort(array, v[0], v[1]);
-        answer.push_back(tmpArr[v[2] - 1]);
+        tmpArr.clear();
+        // 커맨드의 범위 기반으로 임시 배열에 삽입
+        for (int i = command[0] - 1; i < command[1]; i++)
+        {
+            tmpArr.push_back(array[i]);
+        }
+        
+        sort(tmpArr.begin(), tmpArr.end());
+        answer.push_back(tmpArr[command[2] - 1]);
     }
-    
     return answer;
 }
