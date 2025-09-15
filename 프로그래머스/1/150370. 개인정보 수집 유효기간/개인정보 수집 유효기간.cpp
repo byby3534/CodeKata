6e334f3675dbea3fe2931ace5953de55e9a70046
byby3534 <82497387+byby3534@users.noreply.github.com>
2025-09-15 14:37:18 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.04 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -5,37 +5,5 @@ using namespace std;
 
 vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
     vector<int> answer;
-    
-    int todayYear = stoi(today.substr(0, 4));
-    int todayMonth = stoi(today.substr(5, 2));
-    int todayDay = stoi(today.substr(8));
-    
-    int term[20] = {};
-    for (int i = 0; i < terms.size(); i++)
-    {
-        char term_ch = terms[i][0];
-        int date = stoi(terms[i].substr(2));
-        
-        term[term_ch - 'A'] = date * 28;
-    }
-    
-    long total = todayYear * 12 * 28 + todayMonth * 28 + todayDay;
-    
-    for(int i=0; i<privacies.size(); i++) {
-        
-        int tmpYear = stoi(privacies[i].substr(0, 4));
-        int tmpMonth = stoi(privacies[i].substr(5, 2));
-        int tmpDay = stoi(privacies[i].substr(8));
-        
-        char termChar = privacies[i][11];
-        
-        long tmpTotal = tmpYear * 12 * 28 + tmpMonth * 28 + tmpDay;
-        
-        long diff = total - tmpTotal;
-        
-        if(diff >= term[termChar-'A']) answer.push_back(i + 1);
-        else continue; 
-    }
-    
     return answer;
 }
