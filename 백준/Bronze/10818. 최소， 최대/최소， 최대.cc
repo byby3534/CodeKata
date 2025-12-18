acd3d93b8e92b7ab11358b28f817555dd694fd41
byby3534 <82497387+byby3534@users.noreply.github.com>
2025-12-18 23:51:49 +0900

    Optimize min and max calculation in C++ program
    Refactor code to improve efficiency and readability.

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,3 +1,15 @@
+/*
+ * 문제: 백준 10818번 (최소, 최대)
+ * 날짜: 2025-12-18
+ * 풀이 시간: 10분
+ * 시간 복잡도: O(N)
+ * 공간 복잡도: O(1)
+ * 핵심 포인트:
+ * 1. N이 100만이라 입출력 최적화(ios_base::sync_with_stdio) 와 (cin.tie(false)) 필수.
+ * 2. 음수 입력을 고려해 초기값을 INT_MIN, INT_MAX로 설정.
+ * 3. 별도 배열 저장 없이 입력과 동시에 비교하여 공간 효율성 높임.
+ * 4. min, max 함수를 사용하여 해도 가능하며 vector에 먼저 저장하고 풀기도함. 그럴 경우 공간 복잡도 O(N)으로 변경 됨
+ */
 #include <iostream>
 #include <algorithm>
 
@@ -39,4 +51,5 @@ int main()
     }
     
     cout << my_min << " " << my_max;
+
 }
