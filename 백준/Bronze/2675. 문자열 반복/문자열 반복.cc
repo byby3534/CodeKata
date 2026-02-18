c8cf7dbe2f34cdc767cf4ffb92f6a6575c72eb05
byby3534 <82497387+byby3534@users.noreply.github.com>
2026-02-18 23:35:20 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int r;
+		char s[21] = {};
+
+		cin >> r >> s;
+		for (int i = 0; i < strlen(s); i++)
+		{
+			for (int j = 0; j < r; j++)
+			{
+				cout << s[i];
+			}
+		}
+
+		cout << "\n";
+	}
+    return 0;
+}
